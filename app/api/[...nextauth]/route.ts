c5c3cef2615f7a5107e2c3f55b6a772d0a154373
hywkwon <38093517+hywkwon@users.noreply.github.com>
2025-05-14 13:28:55 +0000

    feat: add next-auth route after fixing catch-all issue

--- app/api/[...nextauth]/route.ts
+++ app/api/[...nextauth]/route.ts
@@ -1,5 +1,5 @@
-import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/google"
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
 
 const handler = NextAuth({
   providers: [
@@ -9,6 +9,6 @@ const handler = NextAuth({
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
-})
+});
 
-export { handler as GET, handler as POST }
+export { handler as GET, handler as POST };
