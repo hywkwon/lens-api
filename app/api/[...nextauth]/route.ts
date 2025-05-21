f06f51a782b591ca374ab111ed40dec3053dd27b
hywkwon <38093517+hywkwon@users.noreply.github.com>
2025-05-21 18:27:34 +0900

    Update route.ts

--- app/api/[...nextauth]/route.ts
+++ app/api/[...nextauth]/route.ts
@@ -1,9 +1,8 @@
 // 파일 경로: app/api/[...nextauth]/route.ts
 
-import NextAuth from "next-auth"; // NextAuth.js를 가져옴
-import GoogleProvider from "next-auth/providers/google"; // 구글 로그인 제공자 사용
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
 
-// 구글 로그인 설정
 const authOptions = {
   providers: [
     GoogleProvider({
@@ -14,5 +13,5 @@ const authOptions = {
   // 추가적인 설정을 여기에 작성할 수 있습니다.
 };
 
-// NextAuth.js를 설정하고, authOptions을 전달함
-export default NextAuth(authOptions);
+export { authOptions }; // 명시적으로 authOptions를 export
+export default NextAuth(authOptions); // default는 authOptions을 전달하는 방식으로 사용
