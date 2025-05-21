14cb1124cd4c52633ace02fa29ae7e76139e1ef4
hywkwon <38093517+hywkwon@users.noreply.github.com>
2025-05-21 18:28:39 +0900

    Update route.ts

--- app/api/[...nextauth]/route.ts
+++ app/api/[...nextauth]/route.ts
@@ -1,17 +1,16 @@
-// 파일 경로: app/api/[...nextauth]/route.ts
+// app/api/[...nextauth]/route.ts
 
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
-const authOptions = {
+const handler = NextAuth({
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID, // 구글 클라이언트 ID
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET, // 구글 클라이언트 시크릿
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
-  // 추가적인 설정을 여기에 작성할 수 있습니다.
-};
+  // 다른 설정들이 여기에 들어갈 수 있습니다.
+});
 
-export { authOptions }; // 명시적으로 authOptions를 export
-export default NextAuth(authOptions); // default는 authOptions을 전달하는 방식으로 사용
+export { handler as GET, handler as POST }; // Next.js 13에서는 GET과 POST 메서드를 명시적으로 export
